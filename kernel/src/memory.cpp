d4a723dbfef2c980a82c9193e247418c484c846f
Goktug Yuksel <goktug7913@gmail.com>
2021-07-07 11:22:48 +0300

    virtual memory

--- kernel/src/memory.cpp
+++ kernel/src/memory.cpp
@@ -13,3 +13,9 @@ uint64_t GetMemSize(EFI_MEMORY_DESCRIPTOR* MemMap, uint64_t MemMapEntries, uint6
     
     return totalmembytes;
 }
+
+void memset(void* start, uint8_t val, uint64_t num){
+    for (uint64_t i = 0; i < num; i++){
+        *(uint8_t*)((uint64_t)start + i) = val;
+    }
+}
